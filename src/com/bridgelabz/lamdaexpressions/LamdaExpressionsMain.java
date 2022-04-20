abd96c5e104aad603493c3bc63558120dd64ed85
AshokKoda <a.koda59@gmail.com>
2022-04-20 18:48:28 +0530

    Added:UC1.2 - Show the results of math operations

--- src/com/bridgelabz/lamdaexpressions/LamdaExpressionsMain.java
+++ src/com/bridgelabz/lamdaexpressions/LamdaExpressionsMain.java
@@ -8,12 +8,17 @@ public class LamdaExpressionsMain {
 		IMathFunction add = (x, y) -> x + y;
 
 		System.out.println("Addition is : 5+4 = " + add.calculate(5, 4));
-		
+
 		IMathFunction multiply = (x, y) -> x * y;
 		System.out.println("Multiplication is : 7*8 = " + multiply.calculate(7, 8));
 
 		IMathFunction division = (x, y) -> x / y;
 		System.out.println("Division is : 50/5 = " + division.calculate(50, 5));
+
+		System.out.println("<-------------------------------------------------------->");
+		IMathFunction.printResult(14, 17, "Addition", add);
+		IMathFunction.printResult(12, 11, "Multiplication", multiply);
+		IMathFunction.printResult(500, 100, "Division", division);
 
 	}
 
