96739d67f944abdb14a1850ee973d23e2275330b
AshokKoda <a.koda59@gmail.com>
2022-04-20 18:37:21 +0530

    Added:Welcome to Lamda expressions

---
+++ src/com/bridgelabz/lamdaexpressions/LamdaExpressionsMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.lamdaexpressions;
+
+public class LamdaExpressionsMain {
+
+	public static void main(String[] args) {
+		System.out.println("**************Welcome to Lamda Expressions**************");
+
+	}
+
+}
